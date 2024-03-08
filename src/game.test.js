9075aa26a11591a2a4be2800e11c2ef7b6646210
Edward Rodriguez <36542201+Edward-Rodriguez@users.noreply.github.com>
2024-03-08 08:34:31 -0500

    test: add unit tests for the isSunk method

--- src/game.test.js
+++ src/game.test.js
@@ -1,10 +1,30 @@
 const game = require('./game');
 
-const { Ship } = game;
+let Ship;
+
+beforeEach(() => {
+  Ship = game.Ship();
+});
 
 describe('The Ship factory', () => {
-  test('Ship hit once', () => {
-    expect(Ship().hit()).toBe(1);
+  test('ship hit once', () => {
+    expect(Ship.hit()).toBe(1);
+  });
+
+  test('ship(length 3) is sunk after 3 hits', () => {
+    Ship.length = 3;
+    for (let i = 0; i < 3; i += 1) {
+      Ship.hit();
+    }
+    expect(Ship.isSunk()).toBeTruthy();
+  });
+
+  test('ship(length 3) is not sunk after a few hits', () => {
+    Ship.length = 3;
+    for (let i = 0; i < 2; i += 1) {
+      Ship.hit();
+    }
+    expect(Ship.isSunk()).toBeFalsy();
   });
 
   test.todo('Sunk');
